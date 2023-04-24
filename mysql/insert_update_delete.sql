f434dfba709fc5956faffa973280676d963b00ad
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-04-24 15:38:45 +0200

    insert

---
+++ mysql/insert_update_delete.sql
@@ -0,0 +1,34 @@
+
+create database if not exists hbo;
+
+-- CRUD: Create, Read, Update, Delete
+
+-- this is creating the table schema
+create table if not exists hbo.actor (
+    actor_id int not null auto_increment,
+    first_name varchar(45) not null,
+    last_name varchar(45) not null,
+    last_update timestamp not null default current_timestamp on update current_timestamp,
+    primary key (actor_id)
+);
+
+
+-- Create data
+insert into hbo.actor
+(first_name, last_name)
+values ('Jim', 'Carrey');
+
+-- Read data
+select * from hbo.actor;
+
+select * from hbo.actor
+where actor_id = 1;
+
+-- Update data
+update hbo.actor
+set first_name = 'James'
+where actor_id = 1;
+
+-- Delete
+delete from hbo.actor
+where actor_id = 1;
