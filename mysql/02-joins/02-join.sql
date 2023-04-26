db8bd971bfa9bcbd11b5afd7f2e3684ce7e13a0f
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-04-26 14:44:58 +0200

    This branch is so dirty, even your mom can't clean it.

--- mysql/02-joins/02-join.sql
+++ mysql/02-joins/02-join.sql
@@ -19,3 +19,36 @@ create table school.student_class (
   foreign key (ClassId) references school.classes(ClassId),
   foreign key (StudentId) references school.students(StudentId)
 );
+
+insert into school.students
+(StudentName)
+  values
+('John'),
+('Matt'),
+('James'),
+('Chris');
+
+insert into school.classes
+(ClassName)
+  values
+('Math'),
+('Art'),
+('History');
+
+insert into school.student_class
+(ClassId, StudentId)
+  values
+(1,1),
+(1,2),
+(3,1),
+(3,2),
+(3,3);
+
+select * from school.students;
+select * from school.classes;
+select * from school.student_class;
+
+-- 1. Which Student is attended to any course? We need the Name of the student
+select StudentName from school.student_class
+inner join school.students
+
